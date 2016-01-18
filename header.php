4933b4c14f534e592081f41a4ab9fbf2e28f1182
Richard McCartney <richard@mccartney.io>
2016-01-18 12:41:51 +0000

    Removing build styles, adding the theme style.css file and updating the file path

--- header.php
+++ header.php
@@ -16,6 +16,7 @@
 <meta name="viewport" content="width=device-width, initial-scale=1">
 <link rel="profile" href="http://gmpg.org/xfn/11">
 <link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>">
+<link rel="stylesheet" href="<?php bloginfo('template_directory'); ?>/static/css/style.min.css" type="text/css" media="all" />
 
 <?php wp_head(); ?>
 </head>
