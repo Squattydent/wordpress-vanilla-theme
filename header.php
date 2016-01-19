43d02d92208b2abc2c37cc997abb23a0d5cfc2c1
Richard McCartney <richard@mccartney.io>
2016-01-19 10:09:52 +0000

    Styles update

--- header.php
+++ header.php
@@ -16,7 +16,7 @@
 <meta name="viewport" content="width=device-width, initial-scale=1">
 <link rel="profile" href="http://gmpg.org/xfn/11">
 <link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>">
-<link rel="stylesheet" href="<?php bloginfo('template_directory'); ?>/static/css/style.min.css" type="text/css" media="all" />
+<link rel="stylesheet" href="<?php get_theme_root_uri(); ?>/static/css/build.min.css" media="screen" title="no title" charset="utf-8">
 
 <?php wp_head(); ?>
 </head>
